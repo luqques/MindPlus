e90f89735fa72fcbe350e07f4f77aa919be0b0fe
Lucas da Cunha <luques.cunha@gmail.com>
2024-01-12 13:57:17 -0300

    Realocado projetos e pastas e desenvolvimento do domínio de Usuário e Empresa

---
+++ backend/src/MindPlus/MindPlus/Empresa/Empresa.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MindPlus.Empresa
+{
+    public class Empresa
+    {
+        public string Id { get; set; }
+
+
+        public Empresa(string id)
+        {
+            Id = id;
+        }
+    }
+}
