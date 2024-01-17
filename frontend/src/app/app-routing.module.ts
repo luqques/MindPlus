516f071fc2ffb313b6ba4e92d08791a9f773c56b
luqques <luques.cunha@gmail.com>
2024-01-17 19:17:41 -0300

    Criando route para componente de perfil

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './components/pages/login/login.component';
 import { HomeComponent } from './components/pages/home/home.component';
+import { PerfilComponent } from './components/pages/perfil/perfil.component';
 
 const routes: Routes = [
-  { path: 'login', component: LoginComponent },
   { path: '', component: HomeComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'perfil', component: PerfilComponent },
 ];
 
 @NgModule({
