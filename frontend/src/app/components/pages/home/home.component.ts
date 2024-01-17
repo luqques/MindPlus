55aaee6b688722c648b24ebf1eac2b3d347d5db0
paola.kloch <kloch.157@gmail.com>
2024-01-16 21:30:54 -0300

    feat: starting homepage

--- frontend/src/app/components/pages/home/home.component.ts
+++ frontend/src/app/components/pages/home/home.component.ts
@@ -1,3 +1,4 @@
+// home.component.ts
 import { Component } from '@angular/core';
 
 @Component({
@@ -6,5 +7,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
+  cards = [
+    {
+      src: 'assets/img/1.png',
+      title: 'Conscientização',
+      description: `Treinamentos regulares para gestores e colaboradores sobre sinais
+      de estresse, ansiedade e depressão, bem como estratégias para
+      lidar com essas questões.`,
+      actionText: 'Treinamentos'
+    },
+    // Repeat for other cards
+  ];
 
+  quote = {
+    imageSrc: 'https://www.rpinfo.com.br/views/geral/img/aspas.png',
+    text: 'Só a experiência própria é capaz de tornar sábio o ser humano.',
+    author: 'Sigmund Freud'
+  };
 }
