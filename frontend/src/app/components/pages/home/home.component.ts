cb60e84a1dd0f11a8b973c50342ab4855f4d6b49
paola.kloch <kloch.157@gmail.com>
2024-01-16 21:55:35 -0300

    fix: fixing homepage pictures

--- frontend/src/app/components/pages/home/home.component.ts
+++ frontend/src/app/components/pages/home/home.component.ts
@@ -1,5 +1,5 @@
-// home.component.ts
 import { Component } from '@angular/core';
+
 
 @Component({
   selector: 'app-home',
@@ -9,14 +9,39 @@ import { Component } from '@angular/core';
 export class HomeComponent {
   cards = [
     {
-      src: 'assets/img/1.png',
-      title: 'Conscientização',
-      description: `Treinamentos regulares para gestores e colaboradores sobre sinais
+      src: 'assets/images/mulher-estressadaa.png',
+      title: "Conscientização",
+      description: ` Treinamentos regulares para gestores e colaboradores sobre sinais
       de estresse, ansiedade e depressão, bem como estratégias para
       lidar com essas questões.`,
-      actionText: 'Treinamentos'
+      actionText: "Treinamentos",
     },
-    // Repeat for other cards
+    {
+      src: 'assets/images/mulher-agua.jpg',
+      title: "Acesso a recursos",
+      description: ` Possuímos recursos internos e externos para suporte à saúde
+      mental, como linhas diretas de ajuda, profissionais de saúde
+      mental e aplicativos. Além de uma lista de leituras, vídeos e
+      recursos online que promovem a saúde mental.`,
+      actionText: "Experimente aqui",
+    },
+    {
+      src: 'assets/images/mulher-estressadaa.png',
+      title: "Avaliações Regulares",
+      description: ` Implementamos avaliações regulares da saúde mental para entender
+      as necessidades específicas de nossos colaboradores. Utilizamos
+      pesquisas anônimas para avaliar o nível de estresse, satisfação e
+      bem-estar no trabalho.`,
+      actionText: "Avalie também!",
+    },
+    {
+      src: 'assets/images/mulher-estressadaa.png',
+      title: " Cultura de Apoio",
+      description: ` Valorizamos o equilíbrio entre trabalho e vida pessoal.Encorajamos
+      uma atmosfera de abertura e suporte, onde os colaboradores se
+      sintam à vontade para compartilhar suas preocupações.`,
+      actionText: "Opine",
+    },
   ];
 
   quote = {
