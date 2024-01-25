b4fa3c601811e9e1807bd752c083539cf136cf7b
luqques <luques.cunha@gmail.com>
2024-01-24 21:59:15 -0300

    Alterações na autenticação de usuário.

--- frontend/src/app/components/pages/perfil/perfil.component.ts
+++ frontend/src/app/components/pages/perfil/perfil.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { UsuarioEntity } from 'src/app/interfaces/UsuarioEntity';
 import { LocalStorageService } from 'src/app/services/local-storage/local-storage.service';
@@ -10,17 +10,25 @@ import { UsuarioService } from 'src/app/services/usuario/usuario.service';
   templateUrl: './perfil.component.html',
   styleUrls: ['./perfil.component.css']
 })
-export class PerfilComponent {
-  ngOnInit(): void {}
-
-  constructor(private localStorageService: LocalStorageService, private usuarioService: UsuarioService,) {}
-
-  token: string = this.localStorageService.get('bearerToken')
+export class PerfilComponent implements OnInit {
   
-  usuarioEntity: UsuarioEntity = this.localStorageService.get('usuarioData')
+  usuarioEntity!: UsuarioEntity;
+  
+  ngOnInit(): void {
+    this.usuarioService.obterUsuario(this.usuarioEntity.id).subscribe(usuarioEntityResponse => {
+      this.usuarioEntity = usuarioEntityResponse;
+      console.log(usuarioEntityResponse)
+    });
+  }
 
-  formSubmitted: boolean = false;
+  constructor(private localStorageService: LocalStorageService, private usuarioService: UsuarioService,) {}  
+
+  //formSubmitted: boolean = false;
   async onSubmit(usuarioEntityForm: NgForm) {
-    this.formSubmitted = true;
+    // this.formSubmitted = true;
+
+    // if (usuarioEntityForm.valid) {
+    //   this.usuarioService.obterUsuario(this.usuarioEntity.id)
+    // }
   }
 }
