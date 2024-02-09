502dc42e0d33e539bdf5c1bc9ef0c88dc896e333
Lucas da Cunha <luques.cunha@gmail.com>
2024-02-09 02:17:22 -0300

    Alterações de responsividade e estilo com bootstrap

--- frontend/src/app/components/pages/colaboradores/colaboradores.component.ts
+++ frontend/src/app/components/pages/colaboradores/colaboradores.component.ts
@@ -1,31 +1,42 @@
 import { Component, OnInit } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
 import { UsuarioService } from 'src/app/services/usuario/usuario.service';
 import { IUsuarioEntity } from 'src/app/interfaces/IUsuarioEntity';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-colaboradores',
   templateUrl: './colaboradores.component.html',
   styleUrls: ['./colaboradores.component.css'],
 })
-export class ColaboradoresComponent {
-  
+export class ColaboradoresComponent implements OnInit {
   usuarios!: IUsuarioEntity[];
 
-  constructor(
-    private httpClient: HttpClient,
-    private usuarioService: UsuarioService,
-    ) {}
+  constructor(private usuarioService: UsuarioService) {}
 
   ngOnInit() {
     this.usuarioService.listarUsuarios().subscribe(
       (response) => {
         this.usuarios = response
         console.log(this.usuarios);
+        this.usuariosFiltrados = this.usuarios;
       },
       (error) => {
         console.log('Erro ao listar usuários', error);
       }
     )
+  }
+
+  nomeFiltrado = '';
+  usuariosFiltrados!: IUsuarioEntity[];
+  
+  filtrarUsuarios() {
+    if (this.nomeFiltrado.length > 0) {
+      this.usuariosFiltrados = this.usuarios.filter(usuario => {
+        const nomeFiltradoToLower = this.nomeFiltrado.toLowerCase();
+        return usuario.nome.toLowerCase().includes(nomeFiltradoToLower);
+      });
+    } else {
+      this.usuariosFiltrados = this.usuarios;
+    }
   }
 }
