4ae615dcc69566aa76920849823e40d61ceb2e79
IsabelleVolaco <isabelletvolaco@gmail.com>
2024-02-16 13:26:02 -0300

    entity da avaliação

--- backend/src/MindPlus/MindPlus.Api/Entity/Avaliacao/AvaliacaoEntity.cs
+++ backend/src/MindPlus/MindPlus.Api/Entity/Avaliacao/AvaliacaoEntity.cs
@@ -1,16 +1,11 @@
-/*
-namespace MindPlus.Api.Dto.Usuario
+namespace MindPlus.Api.Entity.Avaliacao
 {
-    public class UsuarioDto
+    public class AvaliacaoEntity
     {
-        public string Nome { get; set; }
-        public string Email { get; set; }
-        public string Senha { get; set; }
-        public string Telefone { get; set; }
-        public string Endereco { get; set; }
-        public int EmpresaId { get; set; }
-        public string Status { get; set; }
-        public string Funcao { get; set; }
+        public int Id { get; set; }
+        public string USUARIO_Id { get; set; }
+        public string Avaliacao { get; set; }
+        public string Data { get; set; }
+        public string Escore { get; set; }
     }
 }
-*/
