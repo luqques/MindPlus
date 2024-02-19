f0a32f31f5ddcd036671402cd20b9f51dc09db38
IsabelleVolaco <isabelletvolaco@gmail.com>
2024-02-18 22:37:03 -0300

    service criado + método de inicialização

---
+++ frontend/src/app/services/avaliacoes/avaliacoes.service.ts
@@ -0,0 +1,43 @@
+import { Injectable } from '@angular/core';
+import { LocalStorageService } from '../local-storage/local-storage.service';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
+import { BehaviorSubject, Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AvaliacaoService {
+  constructor(private http: HttpClient) {}
+
+  private baseApiUrl = environment.baseApiUrl;
+  private apiUrl = `${this.baseApiUrl}avaliacoes`;
+  
+  private userTokenSubject = new BehaviorSubject<string>('');
+
+  public setUserToken(token: string) {
+    this.userTokenSubject.next(token);
+  }
+
+  public userTokenChanged():Observable<string> {
+    return this.userTokenSubject.asObservable();
+  }
+
+  public obterEstatisticas(): Observable<Object> {
+    return this.http.get<Object>(`${this.apiUrl}/estatisticas`, {
+      headers: {
+        Authorization: 
+          this.userTokenSubject.value
+      }
+    });
+  }
+
+  public obterMetas(): Observable<Object> {
+    return this.http.get<Object>(`${this.apiUrl}/metas`, {
+      headers: {
+        Authorization: 
+          this.userTokenSubject.value
+      }
+    });
+  }
+}
