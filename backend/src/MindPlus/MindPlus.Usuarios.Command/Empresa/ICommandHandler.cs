86df0524de91c29172ef9a6a4ef47394a692a8d8
Lucas da Cunha <87662354+luqques@users.noreply.github.com>
2024-02-22 22:18:56 -0300

    Delete backend/src/MindPlus/MindPlus.Usuarios.Command directory

--- backend/src/MindPlus/MindPlus.Usuarios.Command/Empresa/ICommandHandler.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace MindPlus.Command.Empresa
-{
-    public interface ICommandHandler<in TCommandRequest, TCommandResult>
-    {
-        Task<TCommandResult> Handle(TCommandRequest commandRequest);
-
-        Task OnError(Exception exception, TCommandRequest commandRequest);
-    }
-}
