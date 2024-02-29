9708b767d027ee1307253b332a61084699c7e2f3
IsabelleVolaco <isabelletvolaco@gmail.com>
2024-02-29 11:53:44 -0300

    mocks

--- frontend/src/app/components/pages/avaliacoes/admin/admin.component.ts
+++ frontend/src/app/components/pages/avaliacoes/admin/admin.component.ts
@@ -22,20 +22,20 @@ export class AdminComponent implements AfterViewInit {
 
   estatisticasDTO: IEstatisticasDTO = {
     ScoresST: [{ MediaScore: 1, NumeroPessoas: 5 },
-      { MediaScore: 2, NumeroPessoas: 10 },
-      { MediaScore: 3, NumeroPessoas: 15 },
-      { MediaScore: 4, NumeroPessoas: 20 },
-      { MediaScore: 5, NumeroPessoas: 25 }],
+    { MediaScore: 2, NumeroPessoas: 10 },
+    { MediaScore: 3, NumeroPessoas: 15 },
+    { MediaScore: 4, NumeroPessoas: 20 },
+    { MediaScore: 5, NumeroPessoas: 25 }],
     ScoresSP: [{ MediaScore: 1, NumeroPessoas: 5 },
-      { MediaScore: 2, NumeroPessoas: 10 },
-      { MediaScore: 3, NumeroPessoas: 15 },
-      { MediaScore: 4, NumeroPessoas: 20 },
-      { MediaScore: 5, NumeroPessoas: 25 }],
+    { MediaScore: 2, NumeroPessoas: 10 },
+    { MediaScore: 3, NumeroPessoas: 15 },
+    { MediaScore: 4, NumeroPessoas: 20 },
+    { MediaScore: 5, NumeroPessoas: 25 }],
     ScoresRI: [{ MediaScore: 1, NumeroPessoas: 5 },
-      { MediaScore: 2, NumeroPessoas: 10 },
-      { MediaScore: 3, NumeroPessoas: 15 },
-      { MediaScore: 4, NumeroPessoas: 20 },
-      { MediaScore: 5, NumeroPessoas: 25 }],
+    { MediaScore: 2, NumeroPessoas: 10 },
+    { MediaScore: 3, NumeroPessoas: 15 },
+    { MediaScore: 4, NumeroPessoas: 20 },
+    { MediaScore: 5, NumeroPessoas: 25 }],
     NiveisEstresse: new NiveisEstresse(),
     TendenciasTemporais: []
   };
@@ -107,64 +107,65 @@ export class AdminComponent implements AfterViewInit {
       canvas.height = 235;
 
       switch (index) {
-        case 0: 
-        const cAtual = 200;//metasDTO.ColaboradorTotalAtual;
-        const pAtual = 180;//metasDTO.PreenchimentoTotalAtual;
-        console.log(`Colaborador Total Atual: ${cAtual}, Preenchimento Total Atual: ${pAtual}`);
+        case 0:
+          const cAtual = 200;//metasDTO.ColaboradorTotalAtual;
+          const pAtual = 180;//metasDTO.PreenchimentoTotalAtual;
+          console.log(`Colaborador Total Atual: ${cAtual}, Preenchimento Total Atual: ${pAtual}`);
 
-        const data = {
-          labels: ['Total Atual'],
-          datasets: [{
-            label: 'Preenchimento',
-            data: [pAtual],
-            backgroundColor: '#BBED94',
-            //categoryPercentage: 0.7,
-            barThickness: 55,
-            //barPercentage: 0.4,
-          }, {
-            label: 'Colaborador',
-            data: [cAtual],
-            backgroundColor: '#88BBCC',
-            barThickness: 55,
-          }]
-        };
-      
-        new Chart(ctx, {
-          type: 'bar',
-          options: {
-            responsive: false,
-            maintainAspectRatio: false,
-            scales: {
-              x: {
-                
-                stacked: true,
+          const data = {
+            labels: ['Fevereiro'],
+            datasets: [{
+              label: 'Preenchimentos',
+              data: [pAtual],
+              backgroundColor: '#BBED94',
+              //categoryPercentage: 0.7,
+              barThickness: 55,
+              //barPercentage: 0.4,
+            }, {
+              label: 'Colaboradores',
+              data: [cAtual],
+              backgroundColor: '#88BBCC',
+              barThickness: 55,
+            }]
+          };
 
+          new Chart(ctx, {
+            type: 'bar',
+            options: {
+              responsive: false,
+              maintainAspectRatio: false,
+              scales: {
+                x: {
+
+                  stacked: true,
+
+                },
+                y: {
+                  beginAtZero: true,
+
+                },
               },
-              y: {
-                beginAtZero: true,
-                
-              },
+
+
             },
-  
-          
-          },
-          data: data,
-        });
+            data: data,
+          });
           break;
 
         case 1: //PMes
 
-          const pmesLabels: string[] = [];
-          const pmesData: number[] = [];
+          const pmesLabels: string[] = ['Janeiro', 'Fevereiro'];
+          const pmesData: number[] = [172, 180];
 
+          /*
           metasDTO.PreenchimentosMes.forEach(item => {
             pmesData.push(item.Preenchimento);
           
-            // Faça o mapeamento do número do mês para o nome do mês
+            // Fazer o mapeamento do número do mês para o nome do mês
             const nomeMes = this.obterNomeMes(item.Mes);
             pmesLabels.push(nomeMes);
-          });
-          
+          });*/
+
           new Chart(ctx, {
             type: 'bar',
             options: {
@@ -174,25 +175,26 @@ export class AdminComponent implements AfterViewInit {
             data: {
               labels: pmesLabels,
               datasets: [{
+                barThickness: 55,
                 label: 'Quantidade de preenchimentos por mês',
                 data: pmesData,
-                backgroundColor: ['rgb(255, 99, 132)', 'rgb(54, 162, 235)', 'rgb(255, 205, 86)']
+                backgroundColor: ['#2A556B','#88BBCC', ]
               }]
             },
           });
           break;
         case 2: //PAno
 
-        const panoLabels: number[] = [];
-        const panoData: number[] = [];
+          const panoLabels: number[] = [2022,2023,2024];
+          const panoData: number[] = [1988,2112,352];
+          
+          /*metasDTO.PreenchimentosAno.forEach(item => {
+            panoData.push(item.Preenchimento);
 
-        metasDTO.PreenchimentosAno.forEach(item => {
-          panoData.push(item.Preenchimento);
-        
-          // Faça o mapeamento do número do mês para o nome do mês
-          const nomeAno = item.Ano;
-          panoLabels.push(nomeAno);
-        });
+            // Faça o mapeamento do número do mês para o nome do mês
+            const nomeAno = item.Ano;
+            panoLabels.push(nomeAno);
+          });*/
 
           new Chart(ctx, {
             type: 'bar',
@@ -203,9 +205,10 @@ export class AdminComponent implements AfterViewInit {
             data: {
               labels: panoLabels,
               datasets: [{
-                label: 'Quantidade de preenchimentos por ano',
+                barThickness: 55,
+                label: 'Total de preenchimentos por ano',
                 data: panoData,
-                backgroundColor: ['rgb(255, 99, 132)', 'rgb(54, 162, 235)', 'rgb(255, 205, 86)']
+                backgroundColor: ['#2A556B','#5C93AD','#88BBCC']
               }]
             },
           });
@@ -234,23 +237,27 @@ export class AdminComponent implements AfterViewInit {
 
       switch (index) {
         case 0: //SG
-        
+
           // criar lista Data e Labels para cada grafico, nessa lista tera labels 5.4.3.2.1. em data tera o n de pessoas que caiu em cada um desses numeros
 
           //const sgData = estatisticasDTO.ScoresST.map((Score: ScoreAvaliacao) => Score.MediaScore);
           //const sgLabels = estatisticasDTO.ScoresST.map((Score: ScoreAvaliacao) => Score.NumeroPessoas);
+
+          //const sgLabels: number[] = [2022,2023,2024];
+          const sgData: number[] = [15,30,43,68,32];
+          
           const sgLabels = [estatisticasDTO.ScoresRI[0].MediaScore,
           estatisticasDTO.ScoresRI[1].MediaScore,
           estatisticasDTO.ScoresRI[2].MediaScore,
           estatisticasDTO.ScoresRI[3].MediaScore,
           estatisticasDTO.ScoresRI[4].MediaScore];
-
+/*
           const sgData = [Math.round((estatisticasDTO.ScoresRI[0].NumeroPessoas + estatisticasDTO.ScoresSP[0].NumeroPessoas + estatisticasDTO.ScoresST[0].NumeroPessoas) / 3),
           Math.round((estatisticasDTO.ScoresRI[1].NumeroPessoas + estatisticasDTO.ScoresSP[1].NumeroPessoas + estatisticasDTO.ScoresST[1].NumeroPessoas) / 3),
           Math.round((estatisticasDTO.ScoresRI[2].NumeroPessoas + estatisticasDTO.ScoresSP[2].NumeroPessoas + estatisticasDTO.ScoresST[2].NumeroPessoas) / 3),
           Math.round((estatisticasDTO.ScoresRI[3].NumeroPessoas + estatisticasDTO.ScoresSP[3].NumeroPessoas + estatisticasDTO.ScoresST[3].NumeroPessoas) / 3),
           Math.round((estatisticasDTO.ScoresRI[4].NumeroPessoas + estatisticasDTO.ScoresSP[4].NumeroPessoas + estatisticasDTO.ScoresST[4].NumeroPessoas) / 3)];
-
+*/
           new Chart(ctx, {
             type: 'pie',
             options: {
@@ -262,7 +269,7 @@ export class AdminComponent implements AfterViewInit {
               datasets: [{
                 label: 'Quantidade colaboradores',
                 data: sgData,
-                backgroundColor: ['#C43030', '#ED944B', '#E8E766', '#5C93AD','#84BD57'],
+                backgroundColor: ['#C43030', '#ED944B', '#E8E766', '#5C93AD', '#84BD57'],
                 hoverOffset: 4
               }]
             },
@@ -270,12 +277,15 @@ export class AdminComponent implements AfterViewInit {
           break;
 
         case 1: //NE
+        const neData: number[] = [3.58,3.85,2.7,4.2];
+
+        /*
           const neData = [
             estatisticasDTO.NiveisEstresse.MediaGeral,
             estatisticasDTO.NiveisEstresse.MediaGST,
             estatisticasDTO.NiveisEstresse.MediaGSP,
             estatisticasDTO.NiveisEstresse.MediaGRI,
-          ];
+          ];*/
 
           new Chart(ctx, {
             type: 'bar',
@@ -284,39 +294,48 @@ export class AdminComponent implements AfterViewInit {
               maintainAspectRatio: false,
               scales: {
                 y: {
-                  beginAtZero: true, 
+                  beginAtZero: true,
                   max: 5,
-                  reverse: true
+                  
                 }
               }
             },
             data: {
               labels: ['Geral', 'Satisfação no Trabalho', 'Satisfação Pessoal', 'Relações Interpessoais'],
               datasets: [{
+                barThickness: 55,
                 label: 'Média de estresse geral e por avaliação',
                 data: neData,
-                backgroundColor: ['rgb(255, 99, 132)', 'rgb(54, 162, 235)', 'rgb(255, 205, 86)']
+                backgroundColor: ['#2A556B','#5C93AD','#88BBCC','#ADD9E8']
               }]
             },
           });
           break;
         case 2:
 
-          const ttLabels = estatisticasDTO.TendenciasTemporais.map(tt => tt.Mes);
-          const ttData = estatisticasDTO.TendenciasTemporais.map(tt => tt.MediaScore);
+        const ttLabels: number[] = [2022,2023,2024];
+        const ttData: number[] = [3.2,4.0,3.58];
+          //const ttLabels = estatisticasDTO.TendenciasTemporais.map(tt => tt.Mes);
+          //const ttData = estatisticasDTO.TendenciasTemporais.map(tt => tt.MediaScore);
 
           new Chart(ctx, {
             type: 'line',
             options: {
               responsive: false,
-              maintainAspectRatio: false
+              maintainAspectRatio: false,
+              scales: {
+                y: {
+                  beginAtZero: true,
+                  max: 5,
+                }
+              }
             },
             data: {
               labels: ttLabels,
               datasets: [{
                 label: 'Média de score ao longo do tempo',
                 data: ttData,
-                borderColor: 'rgb(255, 99, 132)',
+                borderColor: '#5C93AD',
                 borderWidth: 2,
                 fill: false,
                 pointRadius: 5
@@ -326,23 +345,29 @@ export class AdminComponent implements AfterViewInit {
           break;
         case 3:
 
-
-          const evpData = [estatisticasDTO.NiveisEstresse.MediaGST,
-          estatisticasDTO.NiveisEstresse.MediaGSP];
-
+        const evpData: number[]=[3.85,2.7];
+          //const evpData = [estatisticasDTO.NiveisEstresse.MediaGST, estatisticasDTO.NiveisEstresse.MediaGSP];
 
           new Chart(ctx, {
             type: 'bar',
             options: {
               responsive: false,
-              maintainAspectRatio: false
+              maintainAspectRatio: false,
+              scales: {
+                y: {
+                  beginAtZero: true,
+                  max: 5,
+                  
+                }
+              }
             },
             data: {
               labels: ['Satisfação no Trabalho', 'Satisfação Pessoal'],
               datasets: [{
+                barThickness: 55,
                 label: 'Média de Score entre as duas avaliações',
                 data: evpData,
-                backgroundColor: ['rgb(54, 162, 235)', 'rgb(255, 205, 86)']
+                backgroundColor: ['#88BBCC','#ADD9E8']
               }]
             },
           });
